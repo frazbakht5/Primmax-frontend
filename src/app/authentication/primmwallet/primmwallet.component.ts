6e4dcde8f4aa194f1e7ae72b0a7b0ef8a5afb712
azizjunaid <azizijunaid@gmail.com>
2021-12-28 18:38:00 +0500

    remove un-necessary code

--- src/app/authentication/primmwallet/primmwallet.component.ts
+++ src/app/authentication/primmwallet/primmwallet.component.ts
@@ -7,35 +7,19 @@ import * as AWS from 'aws-sdk';
 import { environment } from '../../../environments/environment';
 
 @Component({
-  selector: 'kyc',
+  selector: 'primmwallet',
   templateUrl: './primmwallet.component.html',
   styleUrls: ['./primmwallet.component.scss'],
 })
 export class PrimmWalletComponent implements OnInit {
-  public form: FormGroup;
-  public isApproved = false;
-  selectedFileSrc: any;
-  selectedFileSrcSelfie: any;
-  imageFileIsTooBig: boolean;
-  uploadImageLabel: string;
-  selectedFile: ImageSnippet;
-  selectedFileSelfie: ImageSnippet;
-  NICImage: File;
-  SelfieImage: File;
+
   public isAdmin = localStorage.getItem('isAdmin') === 'true' ? true : false;
   constructor(
     private router: Router,
     private fb: FormBuilder,
     private commonService: CommonService,
     private api: ApiService
-  ) {
-    AWS.config.update({
-      accessKeyId: environment.S3PicturesAccessKey,
-      secretAccessKey: environment.S3PicturesSecretKey,
-      region: environment.region,
-    });
-    this.getUserDetail();
-  }
+  ) { }
 
   ngOnInit(): void { }
   /**
@@ -96,140 +80,8 @@ export class PrimmWalletComponent implements OnInit {
   public swap() {
     this.router.navigate(['/user/swap']);
   }
-  public getUserDetail() {
-    this.api.httpGet('user').subscribe((response: any) => {
-      console.log('response ===>', response);
-      this.commonService.hideSpinner();
-      if (response && response.code === 200) {
-        this.isApproved =
-          response.data.is_identification_picture_with_selfie_verified;
-      }
-    });
-  }
-
-  async uploadSelfie() {
-    const reader = new FileReader();
-    return new Promise<any>((resolve, reject) => {
-      reader.addEventListener('load', async (event: any) => {
-        console.log('event.target.result', event.target);
-        this.selectedFileSrcSelfie = event.target.result;
-        let userId = localStorage.getItem('userId');
-        let ext = this.SelfieImage.type.split('/');
-        let fileNameId = '' + userId + '-selfie.' + ext[1];
-        const s3 = new AWS.S3();
-        this.selectedFileSelfie = new ImageSnippet(
-          this.selectedFileSrcSelfie,
-          this.SelfieImage
-        );
-        await s3
-          .upload({
-            Bucket: 'primmax-data',
-            Key: `KYC/${userId}/${fileNameId}`,
-            Body: this.selectedFileSelfie.file,
-            ContentType: 'image/jpeg',
-          })
-          .promise();
-        resolve(1);
-      });
-      reader.readAsDataURL(this.SelfieImage);
-    })
-  }
-
-  async uploadNIC() {
-    const reader = new FileReader();
-    return new Promise<any>((resolve, reject) => {
-      reader.addEventListener('load', async (event: any) => {
-        console.log('event.target.result', event.target);
-        this.selectedFileSrc = event.target.result;
-        let userId = localStorage.getItem('userId');
-        let ext = this.NICImage.type.split('/');
-        let fileNameId = '' + userId + '-id.' + ext[1];
-        const s3 = new AWS.S3();
-        this.selectedFile = new ImageSnippet(this.selectedFileSrc, this.NICImage);
-        await s3
-          .upload({
-            Bucket: 'primmax-data',
-            Key: `KYC/${userId}/${fileNameId}`,
-            Body: this.selectedFile.file,
-            ContentType: 'image/jpeg',
-          })
-          .promise();
-        resolve(1);
-      });
-      reader.readAsDataURL(this.NICImage);
-    })
-  }
 
 
-
-  public async update() {
-    this.commonService.showSpinner();
-    try {
-      await this.uploadNIC();
-      await this.uploadSelfie();
-      this.commonService.hideSpinner();
-    } catch (error) {
-      this.commonService.hideSpinner();
-      this.commonService.failureToast("Upload Error", "Something went wrong while uploading");
-    }
-
-
-  }
-
-  changeImageNIC(imageInput: any) {
-    const file: File = imageInput.files[0];
-    this.uploadImageLabel = `${file.name} (${(file.size * 0.000001).toFixed(
-      2
-    )} MB)`;
-    if (file.size > 1048576) {
-      this.imageFileIsTooBig = true;
-    } else {
-      this.imageFileIsTooBig = false;
-    }
-
-    this.NICImage = file;
-  }
-
-  changeImageSelfie(imageInput: any) {
-    const file: File = imageInput.files[0];
-    this.uploadImageLabel = `${file.name} (${(file.size * 0.000001).toFixed(
-      2
-    )} MB)`;
-    if (file.size > 1048576) {
-      this.imageFileIsTooBig = true;
-    } else {
-      this.imageFileIsTooBig = false;
-    }
-
-    this.SelfieImage = file;
-  }
-
-  public onSubmit() {
-    if (this.form.valid) {
-      console.log('form values ===>', this.form.value);
-      localStorage.setItem('username', this.formControl.username.value);
-      this.api
-        .httpPost('user/signup', this.form.value)
-        .subscribe((response: any) => {
-          this.commonService.hideSpinner();
-          if (response && response.code === 200) {
-            this.commonService.successToast(
-              'success',
-              'Email Sent Successfully!'
-            );
-            this.router.navigate(['/verification']);
-          } else {
-            this.commonService.failureToast('Error', response.message);
-          }
-        });
-    }
-  }
-
-  get formControl() {
-    return this.form.controls;
-  }
+  public onSubmit() { }
 }
 
-class ImageSnippet {
-  constructor(public src: string, public file: File) { }
-}
